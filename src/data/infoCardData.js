8e32e95a6ee263a624578cbfb9315a879c6c8b03
Qianye Lin <qianyelin@Qianyes-MBP.hub>
2022-10-16 23:17:54 +1300

    add infoCard data

---
+++ src/data/infoCardData.js
@@ -0,0 +1,30 @@
+export const infoCardData = [
+  {
+    name: "feralGoat",
+    subHeading:
+      "Feral goats can quickly destroy all vegetation within their reach, threatening native plants and damaging the forest understorey.",
+    description:
+      "Goats were introduced to New Zealand in the early days of European settlement for food, to establish a commercial fibre industry, and for weed control on developing land. The descendents of those that escaped or were deliberately released thrived in the country’s grass hills, forest and scrubland areas. Today feral goats (Capra hircus) occur on both main islands and a few offshore islands. Feral goats are classifed as wild animals under the Wild Animal Control Act 1977.",
+  },
+  {
+    name: "stoat",
+    subHeading:
+      "Stoats are the number-one killer of many of New Zealand’s endangered native species.",
+    description:
+      "The effect of stoat predation on the survival of many of New Zealand’s bird species cannot be underestimated. They are voracious and relentless hunters, described as having only two reasons for living – to eat and to reproduce. Stoats are known to be having a significant effect on birds species such as wrybills, the New Zealand dotterel, black-fronted terns and young kiwi. Birds that nest in holes in tree trunks such as mohua, kākā and yellow-crowned kākāriki are easy prey for stoats who can take out eggs, chicks and incubating adults in one attack. Stoats are implicated in the extinction of South Island subspecies of bush wren, laughing owl and New Zealand thrush. Even a 3 kg takahe or 2 kg kakapo can be killed by a stoat, that also has a strategy of killing everything in sight and storing the surplus for later. Stoats and rats are part of a complex predator-prey relationship associated with beech tree seed production. In a periodic ‘mast event’ of beech trees – where high levels of seed production occurs – stoat populations explode assisted by the increased food supply. Later, when the seed supplies run out, the higher numbers of predators have an even greater effect on populations of birds, weta, bats and landsnails.",
+  },
+  {
+    name: "possom",
+    subHeading:
+      "The possum is one of the greatest threats to our natural environment.",
+    description:
+      "Possums have a significant impact on many of New Zealand’s natural ecosystems. They occur in high numbers and their own predators, such as feral cats, do not have much effect on controlling possum population size. Leaves are the main part of their diet, but possums are opportunistic omnivores. They eat buds, flowers, fruit/berries and nectar, which means they compete with native birds and reptiles for food sources. The growth and life-cycle of a tree or plant is significantly affected when all parts of it are eaten. Possums also have ‘favourites’ such as rātā or kamahi trees, leading to an even greater impact on these species. In 1993, possums were filmed eating the eggs and chicks of kōkako and this evidence changed many people’s views of their threat to wildlife. They eat invertebrates, including weta, and are significant predators of New Zealand land snails such as Powelliphanta. They often occupy holes in tree trunks for their nests which would otherwise be used by nesting birds such as kākāriki and saddlebacks. ",
+  },
+  {
+    name: "deer",
+    subHeading:
+      "In New Zealand, deer have no natural predators. This means they can grow to large numbers without the support of hunters and others to aid in their management.",
+    description:
+      "Deer populations have been building over the past couple of decades across the country. This is because they have been harvesting at a lower level than their breeding rate. As numbers have increased, deer have moved into new areas to search for food. Deer damage native forests by feeding on forest plants, trees, and seedlings. They start with the native plants they prefer most such as schefflera, broadleaf, and hen and chicken fern in forest understoreys. Then they will move on to plants they like less. This can result in large groups of deer mainly eating falling leaves from canopy trees. In subalpine areas, deer can damage tall tussocks and wildflowers like alpine buttercups. By targeting these plants and altering the forest structure, deer can change their composition of plants. This takes vital food and shelter from other animals. Over time this can hinder or stop forest regeneration.",
+  },
+];
