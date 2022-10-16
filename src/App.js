fdc0fae5ded477b740712e8e52efd07c3d3daf98
Qianye Lin <qianyelin@Qianyes-MBP.hub>
2022-10-16 14:29:46 +1300

    integrate components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import PestScene from './views/PestScene'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./views/Home";
 
 function App() {
   return (
@@ -8,12 +8,12 @@ function App() {
       <div className="App">
         <div id="canvas-container">
           <Routes>
-            <Route path="/" element={<PestScene />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         </div>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/views/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MapDrawer from "./MapDrawer";
+import PestScene from "./PestScene";
+
+const Home = () => {
+  return (
+    <>
+      <MapDrawer />
+      <PestScene />
+    </>
+  );
+};
+
+export default Home;
