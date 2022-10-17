4e19bf78ac5bc3c83624e967eab232ce56f83b2a
Qianye Lin <qianyelin@Qianyes-MBP.hub>
2022-10-17 14:02:11 +1300

    remove starts

--- src/views/PestScene.jsx
+++ src/views/PestScene.jsx
@@ -33,19 +33,8 @@ const PestScene = ({ currentPest, setCurrentPest, setShowCard }) => {
         inclination={0}
         azimuth={0.25}
       />
-
       <ambientLight intensity={0.2} />
       <directionalLight color="orange" position={[0, 0, 6]} />
-
-      <Stars
-        radius={100} // Radius of the inner sphere (default=100)
-        depth={50} // Depth of area where stars should fit (default=50)
-        count={50000} // Amount of stars (default=5000)
-        factor={4} // Size factor (default=4)
-        saturation={0} // Saturation 0-1 (default=0)
-        fade
-        speed={1} // Faded dots (default=false)
-      />
       <Animals
         currentPest={currentPest}
         setCurrentPest={setCurrentPest}
