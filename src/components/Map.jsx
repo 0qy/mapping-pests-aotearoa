8396d8ed00155fc32e5fb46c55052a0790e497d6
Sophia Lawler <sophialawler@Sophias-MBP.hub>
2022-10-19 14:36:59 +1300

    shadow set to false

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -36,7 +36,7 @@ function Map({ currentPest }) {
     timestamp: Date.UTC(2019, 7, 1, 22),
     color: [255, 255, 255],
     intensity: 1.0,
-    _shadow: true,
+    _shadow: false,
   });
 
   const [effects] = useState(() => {
